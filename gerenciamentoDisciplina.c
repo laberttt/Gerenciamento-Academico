39292d53d01351155eca1bd377d859206f7f50ec
laberttt <labert.ribeiroo@gmail.com>
2024-10-19 10:03:43 -0300

    função imprimir aluno

--- gerenciamentoDisciplina.c
+++ gerenciamentoDisciplina.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-// Estrutura que vai armazenar os dados do discente
+// Estrutura que vai armazenar os dados do aluno
 typedef struct {
     long int matricula;
     char nome[40];
@@ -9,4 +9,15 @@ typedef struct {
     float recuperacao;
     float media;
     char situacao[20];
-} tDiscente;
+} tAluno;
+
+// Função para imprimir os dados do aluno
+void imprimeAluno(tAluno aluno) {
+    if (aluno.recuperacao < 0.001) {
+        printf("\t%-12ld %-20s %-6.1f %-6.1f %-6.1f %-11s %-6.1f %-10s\n", 
+        aluno.matricula, aluno.nome, aluno.nota1, aluno.nota2, aluno.nota3, "-", aluno.media, aluno.situacao);
+    } else {
+        printf("\t%-12ld %-20s %-6.1f %-6.1f %-6.1f %-11.1f %-6.1f %-10s\n", 
+        aluno.matricula, aluno.nome, aluno.nota1, aluno.nota2, aluno.nota3, aluno.recuperacao, aluno.media, aluno.situacao);
+    }
+}
