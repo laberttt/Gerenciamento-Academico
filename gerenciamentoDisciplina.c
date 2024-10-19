28db4c6fdeb1fa45c8f3c0d0bf4c6af9b5973406
laberttt <labert.ribeiroo@gmail.com>
2024-10-19 13:05:58 -0300

    update resto do codigo

--- gerenciamentoDisciplina.c
+++ gerenciamentoDisciplina.c
@@ -31,8 +31,8 @@ tAluno * lerAluno(tAluno * retorno) {
     fgets(retorno->nome, sizeof(retorno->nome), stdin);
 
     // Removendo o \n ao final do nome
-    if (retorno->nome[strlen(retorno->nome) - 1] == "\n") {
-        retorno->nome[strlen(retorno->nome) - 1] == "\0";
+    if (retorno->nome[strlen(retorno->nome) - 1] == '\n') {
+        retorno->nome[strlen(retorno->nome) - 1] = '\0';
     }
     return retorno;
 }
@@ -87,8 +87,137 @@ void lerArquivo(char *nomeArquivo) {
     fclose(fp);
 }
 
-int main()
-{
+int main() {
+    char disciplina[50];
+    int qntAlunos, opc;
+    tAluno alunos[30]; // Limite de quantos alunos pode ter
+
+    puts("--------------------------------------------");
+    printf("Bem-vindo(a) ao COntrole de Disciplina\n");
+    puts("--------------------------------------------");
+
+    printf("\tO que deseja fazer?\n");
+    printf("\t[1] - Visualizar Notas\n");
+    printf("\t[2] - Cadastrar Nova Nota\n");
+    scanf("%d", &opc);
+
+    if (opc == 1) {
+        lerArquivo("./relatorioDeControle.txt");
+        return 0;
+    } else {
+        // Leitura do nome da Disciplina
+        printf("Digite o nome da disciplina: ");
+        fgets(disciplina, sizeof(disciplina), stdin);
+
+        // Limpa o buffer de entrada
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF);
+
+        // // Removendo o \n ao final do nome
+        if (disciplina[strlen(disciplina) - 1] == '\n') {
+            disciplina[strlen(disciplina) - 1] = '\0';
+        }
+
+        // Leitura da quantidade de alunos
+        printf("Digite a quantidade de alunos: ");
+        scanf("%d", &qntAlunos);
+
+        // Leitura dos dados dos alunos
+        for (int i = 0; i < qntAlunos; i++) {
+            tAluno alunoTemp; // Serve para ser uma variável temporária para armazenar os dados do aluno
+
+            puts("\n");
+
+            // Leitura dos dados do aluno
+            lerAluno(&alunoTemp);
+
+            // Copia os dados lidos para o array de alunos
+            alunos[i] = alunoTemp;
+        }
+
+        // Leitura das notas dos alunos e calcular a média
+        for (int i = 0; i < qntAlunos; i++) {
+            lerNotas(&alunos[i]);
+
+            // Calcula a media do aluno
+            alunos[i].media = Media(alunos[i]);
+
+            // Verifica se o aluno foi aprovado ou não ou está em recuperação
+            if (alunos[i].media >= 4.0 && alunos[i].media < 7.0) {
+                printf("O aluno %s teve media %.2lf e ficou em recuperacao! Digite a nota da recupercao dele: \n", alunos[i].nome, alunos[i].media);
+                lerRecuperacao(&alunos[i]);
+
+                // Calcula a media final do aluno
+                alunos[i].media = mediaFinal(alunos[i]);
+
+                // Verifica se o aluno foi aprovado ou reprovado na recuperação
+                if (alunos[i].media >= 5.0) {
+                    strcpy(alunos[i].situacao, "Aprovado");
+                } else {
+                    printf("O aluno %s teve media %.2lf e foi reprovado! \n", alunos[i].nome, alunos[i].media);
+                    strcpy(alunos[i].situacao, "Reprovado");
+                }
+
+            // Verifica se o aluno foi reprovado sem poder fazer final
+            } else if (alunos[i].media < 4.0) {
+                printf("O aluno %s teve media %.2lf e foi reprovado! \n", alunos[i].nome, alunos[i].media);
+                strcpy(alunos[i].situacao, "Reprovado");
+
+            // Verifica se o aluno foi aprovado direto
+            } else {
+                strcpy(alunos[i].situacao, "Aprovado");
+            }
+        }
+
+        // Criando relatorioDeControle.txt de saída
+        FILE *fp;
+        fp = fopen("./relatorioDeControle.txt", "a");
+        if (!fp) {
+            printf("Erro na abertura do arquivo.\n");
+            return 1;
+        } else {
+            // Se o arquivo estiver vazio, escreve o cabeçalho
+            fseek(fp, 0, SEEK_END);
+            if (ftell(fp) == 0) {
+                fprintf(fp, "%-12s %-20s %-6s %-6s %-6s %-11s %-6s %-10s\n", "Matricula", "Nome", "Nota1", "Nota2", "Nota3", "Recuperacao", "Media", "Situacao");
+            }
+        }
+        // Loop de iteração dos alunos
+        for (int i = 0; i < qntAlunos; i++) {
+            // Escreve os dados dos alunos no arquivo
+            if (alunos[i].recuperacao < 0.001) {
+                fprintf(fp, "%-12ld %-20s %-6.1f %-6.1f %-6.1f %-10s %-14.1f %-4s\n", alunos[i].matricula, alunos[i].nome, alunos[i].nota1, alunos[i].nota2, alunos[i].nota3, "-", alunos[i].media, alunos[i].situacao);
+            } else{
+                fprintf(fp, "%-12ld %-20s %-6.1f %-6.1f %-6.1f %-10.1f %-14.1f %-4s\n", alunos[i].matricula, alunos[i].nome, alunos[i].nota1, alunos[i].nota2, alunos[i].nota3, alunos[i].recuperacao, alunos[i].media, alunos[i].situacao);
+            }
+        }
+
+        // Fecha o arquivo somente após todas as operações de escrita serem concluídas
+        fclose(fp);
+    }
+
+    /*
+    //Imprime os dados dos alunos
+
+    puts("\n");
+    printf("\t%-12s %-20s %-6s %-6s %-6s %-11s %-6s %-10s\n", "Matricula", "Nome", "Nota1", "Nota2", "Nota3", "Recuperacao", "Media", "Situacao");
+
+    printf("\t" "--------------------------------" "-------------------" "------" "------" "------" "-----------" "------" "---------\n");
     
+    
+    for (int i = 0; i < qntAlunos; i++){
+        imprimeAluno(alunos[i]);
+    }*/
+
+    puts("\n\n\n");
+    puts("\t-----------------------------------------------------");
+    printf("\tRelatorio de Controle de Disciplina\n");
+    puts("\t-----------------------------------------------------");
+    puts("\n");
+
+    //Leitura do arquivo criado
+    lerArquivo("./relatorioDeControle.txt");
+
+
     return 0;
 }
